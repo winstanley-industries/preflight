8cb19a69a0820e2bb050869e4e00ed48b5ea5888
Adam Winstanley <adam@winstanley.industries>
2026-02-03 06:27:19 -0800

    feat(core): add diff data model

--- crates/preflight-core/src/lib.rs
+++ crates/preflight-core/src/lib.rs
@@ -1,13 +1,2 @@
-pub fn hello() -> &'static str {
-    "hello preflight"
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_hello() {
-        assert_eq!(hello(), "hello preflight");
-    }
-}
+pub mod diff;
+pub mod review;
